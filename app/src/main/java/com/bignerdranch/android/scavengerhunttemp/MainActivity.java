c8a333f100a3d1753b5dfc5247591fcbd30f5896
Brendon <brennon.mckeever@gmail.com>
2016-11-04 16:30:53 -0500

    I;ve added some comments

--- app/src/main/java/com/bignerdranch/android/scavengerhunttemp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/scavengerhunttemp/MainActivity.java
@@ -10,6 +10,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //commentsss
+        //So far so good.
+
+        //TODO First step is Geo Fencing to make sure we can do that.
+
+        //TODO Create a way for the program to track us (assuming the last step doesn't cover that).
+
+        //TODO Enter some dummie data into Firebase and write code to access and modify it.
+
+        //TODO go to a location and have the program confirm we are in the right place.
+
+        // These are the ideas I have so far, your input is greatly welcome.
     }
 }
