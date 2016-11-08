87a5dfb202391f9cc8e282219c7ba4dba7f271fa
miked <whatthafunk3@aol.com>
2016-11-08 15:01:45 -0600

    new layout

---
+++ app/src/main/java/com/bignerdranch/android/scavengerhunttemp/GeoFencing.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.scavengerhunttemp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by miked on 11/8/2016.
+ */
+
+public class GeoFencing extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        ResultCallBack<Status>, {
+
+
+}
