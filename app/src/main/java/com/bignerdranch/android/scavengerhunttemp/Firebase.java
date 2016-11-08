4ba7a81a5a5eae9c1848e2e819efa3d248f4a23d
miked <whatthafunk3@aol.com>
2016-11-08 15:39:00 -0600

    new layout

--- app/src/main/java/com/bignerdranch/android/scavengerhunttemp/Firebase.java
+++ app/src/main/java/com/bignerdranch/android/scavengerhunttemp/Firebase.java
@@ -1,13 +1,20 @@
 package com.bignerdranch.android.scavengerhunttemp;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 /**
  * Created by Brendon on 11/8/16.
  */
 
-public class Firebase {
-
-    private
 
 
 
-}
+
+
