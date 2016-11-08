9768554fd6d51e259668897d9331d23ff66ffef5
Brendon <brennon.mckeever@gmail.com>
2016-11-08 16:44:53 -0600

    Added the Google Json files and updated the Graddle files

--- app/src/main/java/com/bignerdranch/android/scavengerhunttemp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/scavengerhunttemp/MainActivity.java
@@ -1,15 +1,32 @@
 package com.bignerdranch.android.scavengerhunttemp;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Firebase mFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebase = new Firebase();
+
+        getScavengerLists();
+
+
+
+
+    }
+
+
+    private void getScavengerLists() {
+
+        mFirebase.getAllScavengerLists();
 
 
 
