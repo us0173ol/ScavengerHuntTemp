3fb0266c1e9a7bab7a6c6671894b4c9ea0899464
Brendon <brennon.mckeever@gmail.com>
2016-11-08 15:08:25 -0600

    Added the Google Json files and updated the Graddle files

--- app/src/main/java/com/bignerdranch/android/scavengerhunttemp/Firebase.java
+++ app/src/main/java/com/bignerdranch/android/scavengerhunttemp/Firebase.java
@@ -1,12 +1,17 @@
 package com.bignerdranch.android.scavengerhunttemp;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Brendon on 11/8/16.
  */
 
 public class Firebase {
 
-    private
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+
 
 
 
